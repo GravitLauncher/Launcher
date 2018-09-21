5cbc81476f714cedbf83bf3891651d04719edc55
Gravit <gravit.min@ya.ru>
2018-09-21 21:36:23 +0700

    Начало нового протокола

--- LaunchServer/src/main/java/ru/gravit/launchserver/socket/websocket/WebSocketFrameHandler.java
+++ LaunchServer/src/main/java/ru/gravit/launchserver/socket/websocket/WebSocketFrameHandler.java
@@ -1,27 +1,31 @@
 package ru.gravit.launchserver.socket.websocket;
 
-import io.netty.channel.ChannelHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
-import io.netty.util.concurrent.EventExecutorGroup;
-
-import java.util.Locale;
+import io.netty.util.concurrent.GlobalEventExecutor;
+import ru.gravit.utils.helper.IOHelper;
+import ru.gravit.utils.helper.LogHelper;
 
 public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
-
+    static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     @Override
-
+    public void channelActive(ChannelHandlerContext ctx) {
+        LogHelper.debug("New client %s", IOHelper.getIP(ctx.channel().remoteAddress()));
+        channels.add(ctx.channel());
+    }
+    @Override
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
         // ping and pong frames already handled
-        if (frame instanceof TextWebSocketFrame) {
-            // Send the uppercase string back.
-            String request = ((TextWebSocketFrame) frame).text();
-            ctx.channel().writeAndFlush(new TextWebSocketFrame(request.toUpperCase(Locale.US)));
-        } else {
-            String message = "unsupported frame type: " + frame.getClass().getName();
-            throw new UnsupportedOperationException(message);
-        }
+        ByteBuf buf = frame.content();
+        ByteBufInputStream input = new ByteBufInputStream(buf);
+        long handshake = input.readLong();
+        long connection_flags = input.readLong();
+        long type = input.readInt();
+        LogHelper.debug("MessageHead: handshake %dl, flags %dl, type %d", handshake,connection_flags,type);
     }
 }
