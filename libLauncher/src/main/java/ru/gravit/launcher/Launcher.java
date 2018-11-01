4ac26bf3d8fb5ea93475954890150a20ecd0dc9d
Gravit <gravit.min@ya.ru>
2018-11-01 19:22:40 +0700

    4.0.5

--- libLauncher/src/main/java/ru/gravit/launcher/Launcher.java
+++ libLauncher/src/main/java/ru/gravit/launcher/Launcher.java
@@ -64,9 +64,9 @@ public final class Launcher {
     private static final Pattern UUID_PATTERN = Pattern.compile("-", Pattern.LITERAL);
     public static int MAJOR = 4;
     public static int MINOR = 0;
-    public static int PATCH = 4;
+    public static int PATCH = 5;
     public static int BUILD = readBuildNumber();
-    public static Version.Type RELEASE = Version.Type.DEV;
+    public static Version.Type RELEASE = Version.Type.BETA;
 
     @LauncherAPI
     public static LauncherConfig getConfig() {
