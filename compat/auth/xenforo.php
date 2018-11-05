3962041825edc6501bfa34b9f29aeabc28c451a6
Zaxar163 <35835496+Zaxar163@users.noreply.github.com>
2018-11-05 12:50:46 +0300

    Fix ipb.php! (fixed locale). (#42)
    * Fix ipb.php! (fixed locale).
    
    * Fix phpbb.php!
    
    * wordpress.php locale!
    
    * Fix xenforo.php! (locale)
    
    * Update ipb.php
    
    * Fix dle die logging!
    
    * Update dle.php

--- compat/auth/xenforo.php
+++ compat/auth/xenforo.php
@@ -5,7 +5,7 @@ header("Content-Type: text/plain; charset=UTF-8");
 $login = $_GET['login'];
 $password = $_GET['password'];
 if(empty($login) || empty($password)) {
-	exit('Empty login or password');
+	exit('Введены неверные данные');
 }
 
 // Load XenForo core
@@ -20,7 +20,7 @@ $db = XenForo_Application::get('db');
 // Resolve user_id by login
 $result = $db->fetchRow('SELECT user_id, username FROM xf_user WHERE username=' . $db->quote($login) . ' OR email=' . $db->quote($login));
 if(!count($result)) {
-	exit('Incorrect login');
+	exit('Введены неверные данные');
 }
 $user_id = $result['user_id'];
 $username = $result['username'];
@@ -28,7 +28,7 @@ $username = $result['username'];
 // Get user data
 $result = $db->fetchCol('SELECT data FROM xf_user_authenticate WHERE user_id=' . $db->quote($user_id));
 if(!count($result)) {
-	exit('Unable to get user data: ' . $user_id);
+	exit('Внутренняя ошибка'); // rare!
 }
 $data = $result[0];
 
@@ -38,10 +38,10 @@ if(class_exists('XenForo_Authentication_Core12')) {
 	$auth = new XenForo_Authentication_Core12;
 } else if(class_exists('XenForo_Authentication_Core')) {
 	$auth = new XenForo_Authentication_Core;
-} else exit('Unable to select authentication core');
+} else exit('Внутренняя ошибка'); // rare!
 
 // Try authenticate
 $auth->setData($data);
 $success = $auth->authenticate($user_id, $password);
-echo($success ? 'OK:' . $username : 'Incorrect login or password');
+echo($success ? 'OK:' . $username : 'Ошибка при авторизации');
 ?>
