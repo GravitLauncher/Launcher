6a9ce59a9400009b351df42f1fff2adcdaf818ad
Zaxar163 <35835496+Zaxar163@users.noreply.github.com>
2018-11-05 12:33:20 +0300

    Fix ipb.php! (fixed locale).

--- compat/auth/ipb.php
+++ compat/auth/ipb.php
@@ -5,13 +5,13 @@ header("Content-Type: text/plain; charset=UTF-8");
 $login = $_GET['login'];
 $password = $_GET['password'];
 if(empty($login) || empty($password)) {
-	exit('Empty login or password');
+	exit('Введены неверные данные');
 }
 
 // Load IPB core
 define('IPB_THIS_SCRIPT', 'public');
-require_once('initdata.php');
-require_once(IPS_ROOT_PATH . 'sources/base/ipsRegistry.php');
+require('initdata.php'); // not once!!!
+require(IPS_ROOT_PATH . 'sources/base/ipsRegistry.php');
 $reg = ipsRegistry::instance();
 $reg->init();
 
@@ -19,10 +19,10 @@ $reg->init();
 $member = IPSMember::load(IPSText::parseCleanValue($login), 'all', 'username');
 $member_id = $member['member_id'];
 if (!$member_id) {
-	exit('Incorrect login');
+	exit('Введены неверные данные');
 }
 
 // Try authenticate
 $success = IPSMember::authenticateMember($member_id, md5(IPSText::parseCleanValue($password)));
-echo($success ? 'OK:' . $member['name'] : 'Incorrect login or password');
+echo($success ? 'OK:' . $member['name'] : 'Ошибка при авторизации');
 ?>
