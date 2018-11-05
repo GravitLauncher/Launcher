b5848c10e62f181ead9be0c40bc716db7969856d
Zaxar163 <35835496+Zaxar163@users.noreply.github.com>
2018-11-05 12:38:04 +0300

    wordpress.php locale!

--- compat/auth/wordpress.php
+++ compat/auth/wordpress.php
@@ -5,11 +5,11 @@ header("Content-Type: text/plain; charset=UTF-8");
 $login = $_GET['login'];
 $password = $_GET['password'];
 if(empty($login) || empty($password)) {
-	exit('Empty login or password');
+	exit('Введены неверные данные');
 }
 
 // Try authenticate
 require_once('wp-load.php');
 $user = wp_authenticate($login, $password);
-echo($user instanceof WP_User ? 'OK:' . $user->user_login : 'Incorrect login or password');
+echo($user instanceof WP_User ? 'OK:' . $user->user_login : 'Ошибка при авторизации');
 ?>
