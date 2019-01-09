8d8eb2e9a3b260208c08fac254a06dbab24302d5
Gravit <gravit.min@ya.ru>
2019-01-10 03:36:02 +0700

    Опечатка ConfigCommand

--- LaunchServer/src/main/java/ru/gravit/launchserver/command/service/ConfigCommand.java
+++ LaunchServer/src/main/java/ru/gravit/launchserver/command/service/ConfigCommand.java
@@ -22,7 +22,7 @@ public class ConfigCommand extends Command {
     @Override
     public void invoke(String... args) throws Exception {
         verifyArgs(args,2);
-        LogHelper.info("Call %s module % action",args[0],args[1]);
+        LogHelper.info("Call %s module %s action",args[0],args[1]);
         String[] new_args = new String[args.length - 2];
         System.arraycopy(args, 2, new_args, 0, args.length - 2);
         server.reconfigurableManager.call(args[0],args[1],new_args);
