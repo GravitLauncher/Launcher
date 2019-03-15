39cbfb0e331161ec5fd0b7c4ff11b767a5d953e3
zaxar163 <zahar.vcherachny@yandex.ru>
2019-03-15 20:04:26 +0300

    [FIX] Рабочий радон.

--- LaunchServer/src/main/java/ru/gravit/launchserver/binary/JARLauncherBinary.java
+++ LaunchServer/src/main/java/ru/gravit/launchserver/binary/JARLauncherBinary.java
@@ -44,9 +44,9 @@ public final class JARLauncherBinary extends LauncherBinary {
         tasks.add(new PrepareBuildTask(server));
         tasks.add(new MainBuildTask(server));
         tasks.add(new ProGuardBuildTask(server));
+        tasks.add(new AdditionalFixesApplyTask(server));
         tasks.add(new RadonBuildTask(server));
         tasks.add(new AttachJarsTask(server));
-        tasks.add(new AdditionalFixesApplyTask(server));
     }
 
     @Override
