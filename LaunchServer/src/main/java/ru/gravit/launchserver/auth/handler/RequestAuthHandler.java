574f40d3eef9c80de4e11a7aba5d0f4bd2b48ed8
sasha0552 <admin@sasha0552.ru>
2019-04-09 00:32:20 +0600

    Перевод на CachedAuthHandler

--- LaunchServer/src/main/java/ru/gravit/launchserver/auth/handler/RequestAuthHandler.java
+++ LaunchServer/src/main/java/ru/gravit/launchserver/auth/handler/RequestAuthHandler.java
@@ -1,15 +1,15 @@
 package ru.gravit.launchserver.auth.handler;
 
-import ru.gravit.launchserver.auth.provider.AuthProviderResult;
 import ru.gravit.utils.helper.CommonHelper;
 import ru.gravit.utils.helper.IOHelper;
 import ru.gravit.utils.helper.LogHelper;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Map;
 import java.util.UUID;
 
-public final class RequestAuthHandler extends AuthHandler {
+public final class RequestAuthHandler extends CachedAuthHandler {
     private String url;
 
     @Override
@@ -18,30 +18,17 @@ public final class RequestAuthHandler extends AuthHandler {
     }
 
     @Override
-    public UUID auth(AuthProviderResult authResult) throws IOException {
-        //TODO вот тут не понял поэтому пусть пока что возращает рандом uuid
-        return UUID.randomUUID();
+    protected Entry fetchEntry(UUID uuid) throws IOException {
+        String uuidTOstring = uuid.toString();
+        return IOHelper.request(new URL(CommonHelper.replace(url, "type", "GetUsername", "uuid", IOHelper.urlEncode(uuidTOstring))));
     }
 
-    @Override
-    public UUID checkServer(String username, String serverID) throws IOException {
-        String currentResponse = IOHelper.request(new URL(CommonHelper.replace(url, "type", "GetUUID", "username", IOHelper.urlEncode(username))));
-        UUID stringTOuuid = UUID.fromString(currentResponse);
-        return stringTOuuid;
-    }
 
-    @Override
-    public boolean joinServer(String username, String accessToken, String serverID) throws IOException {
-        String currentResponse = IOHelper.request(new URL(CommonHelper.replace(url, "type", "GetAll", "username", IOHelper.urlEncode(username), "accessToken", IOHelper.urlEncode(accessToken), "serverID", IOHelper.urlEncode(serverID))));
-        String[] joinServerParams = currentResponse.split(":");
-        if (joinServerParams[0] == username && joinServerParams[1] == accessToken) {
-            if (joinServerParams[2] == serverID || joinServerParams == null) {
-                return true;
-            }
-        }
-        return false;
-    }
 
+
+
+
+    /*
     @Override
     public UUID usernameToUUID(String username) throws IOException {
         String currentResponse = IOHelper.request(new URL(CommonHelper.replace(url, "type", "GetUUID", "username", IOHelper.urlEncode(username))));
@@ -55,7 +42,7 @@ public final class RequestAuthHandler extends AuthHandler {
         String currentResponse = IOHelper.request(new URL(CommonHelper.replace(url, "type", "GetUsername", "uuid", IOHelper.urlEncode(uuidTOstring))));
         return currentResponse;
     }
-
+    */
     @Override
     public void close() {
         // Ничего не делать
