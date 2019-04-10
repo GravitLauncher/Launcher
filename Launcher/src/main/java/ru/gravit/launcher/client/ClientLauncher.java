eab5b350207cb6f3d118bcf878dac90e3a072733
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-04-10 09:54:04 +0300

    Revert "[FIX] Agent работает."
    This reverts commit 76a9c07a4802bae69907e1c27f021d10b3c5a345.

--- Launcher/src/main/java/ru/gravit/launcher/client/ClientLauncher.java
+++ Launcher/src/main/java/ru/gravit/launcher/client/ClientLauncher.java
@@ -366,7 +366,7 @@ public final class ClientLauncher {
         Collections.addAll(context.args, profile.getJvmArgs());
         profile.pushOptionalJvmArgs(context.args);
         Collections.addAll(context.args, "-Djava.library.path=".concat(params.clientDir.resolve(NATIVES_DIR).toString())); // Add Native Path
-        Collections.addAll(context.args, "-javaagent:".concat(pathLauncher).concat("=pr"));
+        Collections.addAll(context.args, "-javaagent:".concat(pathLauncher));
         LauncherGuardManager.guard.addCustomParams(context);
         Collections.addAll(context.args, ClientLauncher.class.getName());
 
