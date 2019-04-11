9e74e1edcfe59db788c5667b90c5eb2d1d697a71
sasha0552 <admin@sasha0552.ru>
2019-04-11 20:12:17 +0600

    Дебаг

--- LaunchServer/src/main/java/ru/gravit/launchserver/auth/handler/RequestAuthHandler.java
+++ LaunchServer/src/main/java/ru/gravit/launchserver/auth/handler/RequestAuthHandler.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 public final class RequestAuthHandler extends CachedAuthHandler {
     private String url;
+	private String SecretKey;
 
     @Override
     public void init() {
@@ -17,25 +18,38 @@ public final class RequestAuthHandler extends CachedAuthHandler {
 
     @Override
     protected Entry fetchEntry(UUID uuid) throws IOException {
-        String username = IOHelper.request(new URL(url + "?type=GetUsername&uuid=" + IOHelper.urlEncode(uuid.toString())));
-        String accessToken = IOHelper.request(new URL(url + "?type=GetAccessToken&uuid=" + IOHelper.urlEncode(uuid.toString())));
-        String serverID = IOHelper.request(new URL(url + "?type=GetServerID&uuid=" + IOHelper.urlEncode(uuid.toString())));
-        return new Entry(uuid, username, accessToken, serverID);
+        String username = IOHelper.request(new URL(url + "?type=GetUsername&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&uuid=" + IOHelper.urlEncode(uuid.toString())));
+        String accessToken = IOHelper.request(new URL(url + "?type=GetAccessToken&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&uuid=" + IOHelper.urlEncode(uuid.toString())));
+        String serverID = IOHelper.request(new URL(url + "?type=GetServerID&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&uuid=" + IOHelper.urlEncode(uuid.toString())));
+		LogHelper.debug("[AuthHandler] Getted username: " + username);
+		LogHelper.debug("[AuthHandler] Getted accessToken: " + accessToken);
+		LogHelper.debug("[AuthHandler] Getted serverID: " + serverID);
+		LogHelper.debug("[AuthHandler] Getted UUID: " + uuid);
+		Entry entr = new Entry(uuid, username, accessToken, serverID);
+        return entr;
     }
 
     @Override
     protected Entry fetchEntry(String username) throws IOException {
-        String GettedUUID = IOHelper.request(new URL(url + "?type=GetUUID&username=" + IOHelper.urlEncode(username)));
+        String GettedUUID = IOHelper.request(new URL(url + "?type=GetUUID&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&username=" + IOHelper.urlEncode(username)));
         UUID uuid = UUID.fromString(GettedUUID);
-        String accessToken = IOHelper.request(new URL(url + "?type=GetAccessToken&username=" + IOHelper.urlEncode(username)));
-        String serverID = IOHelper.request(new URL(url + "?type=GetServerID&username=" + IOHelper.urlEncode(username)));
-        return new Entry(uuid, username, accessToken, serverID);
+        String accessToken = IOHelper.request(new URL(url + "?type=GetAccessToken&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&username=" + IOHelper.urlEncode(username)));
+        String serverID = IOHelper.request(new URL(url + "?type=GetServerID&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&username=" + IOHelper.urlEncode(username)));
+		LogHelper.debug("[AuthHandler] Getted username: " + username);
+		LogHelper.debug("[AuthHandler] Getted accessToken: " + accessToken);
+		LogHelper.debug("[AuthHandler] Getted serverID: " + serverID);
+		LogHelper.debug("[AuthHandler] Getted UUID: " + uuid);
+        Entry entr = new Entry(uuid, username, accessToken, serverID);
+		return entr;
     }
 
     @Override
     protected boolean updateAuth(UUID uuid, String username, String accessToken) throws IOException {
-        String response0 = IOHelper.request(new URL(url+ "?type=SetUUID&uuid=" + IOHelper.urlEncode(uuid.toString()) + "&username=" + IOHelper.urlEncode(username)));
-        String response1 = IOHelper.request(new URL(url+ "?type=SetAccessToken&accessToken=" + IOHelper.urlEncode(accessToken) + "&username=" + IOHelper.urlEncode(username)));
+        String response0 = IOHelper.request(new URL(url+ "?type=SetUUID&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&uuid=" + IOHelper.urlEncode(uuid.toString()) + "&username=" + IOHelper.urlEncode(username)));
+        String response1 = IOHelper.request(new URL(url+ "?type=SetAccessToken&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&accessToken=" + IOHelper.urlEncode(accessToken) + "&username=" + IOHelper.urlEncode(username)));
+		LogHelper.debug("[AuthHandler] Set accessToken: " + accessToken);
+		LogHelper.debug("[AuthHandler] Set UUID: " + uuid);
+		LogHelper.debug("[AuthHandler] For this username: " + username);
         if (response0 == "OK" && response1 == "OK") {
             return true;
         }
@@ -44,8 +58,10 @@ public final class RequestAuthHandler extends CachedAuthHandler {
 
     @Override
     protected boolean updateServerID(UUID uuid, String serverID) throws IOException {
-        String response = IOHelper.request(new URL(url + "?type=SetServerID&uuid=" + IOHelper.urlEncode(uuid.toString()) + "&serverID=" + IOHelper.urlEncode(serverID)));
-        if (response == "OK") {
+        String response = IOHelper.request(new URL(url + "?type=SetServerID&SecretKey=" + IOHelper.urlEncode(SecretKey) + "&uuid=" + IOHelper.urlEncode(uuid.toString()) + "&serverID=" + IOHelper.urlEncode(serverID)));
+		LogHelper.debug("[AuthHandler] Set serverID: " + serverID);
+		LogHelper.debug("[AuthHandler] For this UUID: " + uuid);
+		if (response == "OK") {
             return true;
         }
         return false;
