05539133cbdb90dcf8f1a8be223babea86d863fb
Zaxar163 <35835496+Zaxar163@users.noreply.github.com>
2019-04-27 15:24:20 +0300

    [FEATURE] Поддержка 1.14

--- libLauncher/src/main/java/ru/gravit/launcher/profiles/ClientProfile.java
+++ libLauncher/src/main/java/ru/gravit/launcher/profiles/ClientProfile.java
@@ -28,6 +28,7 @@ public final class ClientProfile implements Comparable<ClientProfile> {
         MC113("1.13", 393),
         MC1131("1.13.1", 401),
         MC1132("1.13.2", 402);
+        MC114("1.14", 477);
         private static final Map<String, Version> VERSIONS;
 
         static {
