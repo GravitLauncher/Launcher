77ac4b9462a9814c164fcbcc1caf5d7f13941acf
Gravit <gravit.min@ya.ru>
2019-05-04 16:14:39 +0700

    [ANY] 5.0.0b5

--- libLauncher/src/main/java/ru/gravit/utils/Version.java
+++ libLauncher/src/main/java/ru/gravit/utils/Version.java
@@ -18,7 +18,7 @@ public final class Version {
     public static final int MAJOR = 5;
     public static final int MINOR = 0;
     public static final int PATCH = 0;
-    public static final int BUILD = 4;
+    public static final int BUILD = 5;
     public static final Version.Type RELEASE = Version.Type.BETA;
 
     @LauncherAPI
