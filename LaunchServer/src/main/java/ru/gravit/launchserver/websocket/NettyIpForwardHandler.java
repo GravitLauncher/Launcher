94b5a07fc92d3b3adef65672c0b472feab47bb71
Gravit <gravit.min@ya.ru>
2019-05-09 22:21:24 +0700

    [FIX] Вернулись назад NettyIpForwardHandler

--- LaunchServer/src/main/java/ru/gravit/launchserver/websocket/NettyIpForwardHandler.java
+++ LaunchServer/src/main/java/ru/gravit/launchserver/websocket/NettyIpForwardHandler.java
@@ -1,15 +1,15 @@
 package ru.gravit.launchserver.websocket;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageCodec;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpRequest;
+import io.netty.util.ReferenceCounted;
 import ru.gravit.utils.helper.LogHelper;
 
 import java.util.List;
 
-public class NettyIpForwardHandler extends MessageToMessageCodec<HttpRequest, HttpRequest> {
+public class NettyIpForwardHandler extends MessageToMessageDecoder<HttpRequest> {
     private NettyConnectContext context;
 
     public NettyIpForwardHandler(NettyConnectContext context) {
@@ -18,28 +18,11 @@ public class NettyIpForwardHandler extends MessageToMessageCodec<HttpRequest, Ht
     }
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, HttpRequest msg, List<Object> out) throws Exception {
-        if (context.ip != null) {
-            out.add(msg);
-            return;
-        }
-        HttpHeaders headers = msg.headers();
-        String realIP = null;
-        if (headers.contains("X-Forwarded-For")) {
-            realIP = headers.get("X-Forwarded-For");
-        }
-        if (headers.contains("X-Real-IP")) {
-            realIP = headers.get("X-Real-IP");
-        }
-        if (realIP != null) {
-            LogHelper.dev("Real IP address %s", realIP);
-            context.ip = realIP;
-        } else LogHelper.error("IpForwarding error. Headers not found");
-        out.add(msg);
-    }
-
-    @Override
     protected void decode(ChannelHandlerContext ctx, HttpRequest msg, List<Object> out) throws Exception {
+        if(msg instanceof ReferenceCounted)
+        {
+            ((ReferenceCounted) msg).retain();
+        }
         if (context.ip != null) {
             out.add(msg);
             return;
