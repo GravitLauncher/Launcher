f12814f5c85b4b2e74fddc6ac5b47856f584bf99
Alex Dev Sm <leshasmlesha@gmail.com>
2019-06-23 16:55:56 +0300

    Забыл добавить import

--- Launcher/src/main/java/pro/gravit/launcher/LauncherEngine.java
+++ Launcher/src/main/java/pro/gravit/launcher/LauncherEngine.java
@@ -10,6 +10,7 @@ import pro.gravit.launcher.client.LauncherUpdateController;
 import pro.gravit.launcher.guard.LauncherGuardManager;
 import pro.gravit.launcher.gui.JSRuntimeProvider;
 import pro.gravit.launcher.gui.RuntimeProvider;
+import pro.gravit.launcher.gui.GuiRuntimeProvider;
 import pro.gravit.launcher.managers.ClientGsonManager;
 import pro.gravit.launcher.managers.ConsoleManager;
 import pro.gravit.launcher.request.Request;
