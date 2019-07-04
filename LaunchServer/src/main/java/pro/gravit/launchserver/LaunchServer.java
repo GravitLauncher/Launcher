95d8cc461c481175262d8eadc084423f750777af
Gravit <gravit.min@ya.ru>
2019-07-04 16:19:07 +0700

    [FIX] Зеркало изменено на https://mirror.gravit.pro

--- LaunchServer/src/main/java/pro/gravit/launchserver/LaunchServer.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/LaunchServer.java
@@ -714,7 +714,7 @@ public final class LaunchServer implements Runnable, AutoCloseable, Reloadable {
         // Create new config
         LogHelper.info("Creating LaunchServer config");
         Config newConfig = new Config();
-        newConfig.mirrors = new String[]{"http://mirror.gravitlauncher.ml/", "https://mirror.gravit.pro/"};
+        newConfig.mirrors = new String[]{"https://mirror.gravit.pro/"};
         newConfig.launch4j = new ExeConf();
         newConfig.launch4j.enabled = true;
         newConfig.launch4j.copyright = "© GravitLauncher Team";
