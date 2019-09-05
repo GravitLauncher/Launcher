a91135835ad68f6c0fe85b2927c49e93724a6868
Gravit <gravit.min@ya.ru>
2019-09-05 18:51:48 +0700

    [FIX] Multiple Definion при использовании ProGuard

--- LauncherAPI/src/main/java/pro/gravit/launcher/events/ExceptionEvent.java
+++ LauncherAPI/src/main/java/pro/gravit/launcher/events/ExceptionEvent.java
@@ -1,12 +1,15 @@
 package pro.gravit.launcher.events;
 
+import pro.gravit.launcher.LauncherNetworkAPI;
+
 public class ExceptionEvent extends RequestEvent {
     public ExceptionEvent(Exception e) {
         this.message = e.getMessage();
         this.clazz = e.getClass().getName();
     }
-
+    @LauncherNetworkAPI
     public final String message;
+    @LauncherNetworkAPI
     public final String clazz;
 
     @Override
--- LauncherAPI/src/main/java/pro/gravit/launcher/events/RequestEvent.java
+++ LauncherAPI/src/main/java/pro/gravit/launcher/events/RequestEvent.java
@@ -2,8 +2,10 @@ package pro.gravit.launcher.events;
 
 import java.util.UUID;
 
+import pro.gravit.launcher.LauncherNetworkAPI;
 import pro.gravit.launcher.request.WebSocketEvent;
 
 public abstract class RequestEvent implements WebSocketEvent {
+    @LauncherNetworkAPI
     public UUID requestUUID;
 }
