20f33b18764ad265d745180582fe974cd3d7954d
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-10-11 15:24:29 +0200

    [FIX] Ещё 1 способ System.exit в обход SecurityManager.

--- Launcher/src/main/java/pro/gravit/launcher/utils/NativeJVMHalt.java
+++ Launcher/src/main/java/pro/gravit/launcher/utils/NativeJVMHalt.java
@@ -1,5 +1,10 @@
 package pro.gravit.launcher.utils;
 
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 import pro.gravit.utils.helper.JVMHelper;
 
 public final class NativeJVMHalt {
@@ -22,13 +27,25 @@ public final class NativeJVMHalt {
         try {
         	JVMHelper.RUNTIME.exit(code);
         } catch (Throwable ignored) {
+        	new WindowShutdown();
         }
         halt.aaabbb38C_D();
         boolean a = halt.aaabBooleanC_D();
         System.out.println(a);
+        
     }
 
     public static boolean initFunc() {
         return true;
+    }
+
+    public static class WindowShutdown extends JFrame {
+		private static final long serialVersionUID = 6321323663070818367L;
+
+		public WindowShutdown() {
+    		super();
+    		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    		super.processWindowEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    	}
     }
 }
