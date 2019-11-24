e9db1b307b5252340553a00ea6e776082bf33e5d
Gravit <gravit.min@ya.ru>
2019-11-25 00:33:18 +0700

    [FEATURE] Multi-Release JAR и компиляция только на Java 11+

--- LauncherCore/src/main/java/pro/gravit/utils/helper/JVMHelper.java
+++ LauncherCore/src/main/java/pro/gravit/utils/helper/JVMHelper.java
@@ -59,6 +59,17 @@ public final class JVMHelper {
 
     public static final ClassLoader LOADER = ClassLoader.getSystemClassLoader();
 
+    public static final int JVM_VERSION = getVersion();
+    public static int getVersion() {
+        String version = System.getProperty("java.version");
+        if(version.startsWith("1.")) {
+            version = version.substring(2, 3);
+        } else {
+            int dot = version.indexOf(".");
+            if(dot != -1) { version = version.substring(0, dot); }
+        } return Integer.parseInt(version);
+    }
+
     static {
         try {
             MethodHandles.publicLookup(); // Just to initialize class
