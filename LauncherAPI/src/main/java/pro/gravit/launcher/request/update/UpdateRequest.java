20913cdbc6d50075c8b2e3ee116c3ccae5b8b154
Gravit <gravit.min@ya.ru>
2019-12-05 02:24:19 +0700

    [FIX] Некорректный UpdateRequest

--- LauncherAPI/src/main/java/pro/gravit/launcher/request/update/UpdateRequest.java
+++ LauncherAPI/src/main/java/pro/gravit/launcher/request/update/UpdateRequest.java
@@ -1,16 +1,17 @@
 package pro.gravit.launcher.request.update;
 
-import pro.gravit.launcher.LauncherAPI;
 import pro.gravit.launcher.LauncherNetworkAPI;
 import pro.gravit.launcher.events.request.UpdateRequestEvent;
 import pro.gravit.launcher.request.Request;
 import pro.gravit.launcher.request.websockets.StandartClientWebSocketService;
 import pro.gravit.launcher.request.websockets.WebSocketRequest;
 import pro.gravit.utils.helper.LogHelper;
-import java.nio.file.Path;
-import java.util.Objects;
 
 public final class UpdateRequest extends Request<UpdateRequestEvent> implements WebSocketRequest {
+
+    public UpdateRequest(String dirName) {
+        this.dirName = dirName;
+    }
 
     @Override
     public String getType() {
@@ -27,14 +28,5 @@ public final class UpdateRequest extends Request<UpdateRequestEvent> implements 
 
     // Instance
     @LauncherNetworkAPI
-    private transient final Path dir;
-
-    public Path getDir() {
-        return dir;
-    }
-
-    @LauncherAPI
-    public UpdateRequest(Path dir) {
-        this.dir = Objects.requireNonNull(dir, "dir");
-    }
+    private final String dirName;
 }
