383551229da63d94bd12d418bab1d2073c56d94d
Gravit <gravita@gravit.pro>
2020-09-08 21:09:15 +0700

    [FEATURE] Поддержка HikariCP в HibernateDAO

--- LaunchServer/src/main/java/pro/gravit/launchserver/command/basic/TestCommand.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/command/basic/TestCommand.java
@@ -1,10 +1,14 @@
 package pro.gravit.launchserver.command.basic;
 
 import org.bouncycastle.cert.X509CertificateHolder;
+import pro.gravit.launcher.request.auth.password.AuthPlainPassword;
 import pro.gravit.launchserver.LaunchServer;
+import pro.gravit.launchserver.auth.AuthException;
+import pro.gravit.launchserver.auth.AuthProviderPair;
 import pro.gravit.launchserver.command.Command;
 import pro.gravit.launchserver.socket.handlers.NettyServerSocketHandler;
 import pro.gravit.utils.helper.CommonHelper;
+import pro.gravit.utils.helper.LogHelper;
 
 import java.nio.file.Paths;
 import java.security.KeyPair;
@@ -53,6 +57,34 @@ public class TestCommand extends Command {
             X509CertificateHolder cert = server.certificateManager.generateCertificate(name, pair.getPublic());
             server.certificateManager.writePrivateKey(Paths.get(name.concat(".key")), pair.getPrivate());
             server.certificateManager.writeCertificate(Paths.get(name.concat(".crt")), cert);
+        }
+        if(args[0].equals("authstresser"))
+        {
+            AuthProviderPair pair = server.config.getAuthProviderPair();
+            AuthPlainPassword plainPassword = new AuthPlainPassword("test");
+            Runnable runnable = () -> {
+                long startTime = System.currentTimeMillis();
+                for(int i=0;i<100000;++i)
+                {
+                    try {
+                        pair.provider.auth("Test", plainPassword, "127.0.0.1");
+                    } catch (AuthException ignored)
+                    {
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    if((i % 10000)  == 0)
+                    {
+                        LogHelper.info("Completed %d requests", i);
+                    }
+                }
+                LogHelper.info("Completed all requests. Time %d ms", System.currentTimeMillis() - startTime);
+            };
+            for(int i=0;i<7;++i)
+            {
+                CommonHelper.newThread(String.format("Stresser #%d", i), true, runnable).start();
+            }
         }
     }
 }
