2688270fa07f2f67b1f138f68eda61bc8e284466
Gravit <gravita@gravit.pro>
2020-11-21 21:30:21 +0700

    [FEATURE] Advanced Client API

--- LaunchServer/src/main/java/pro/gravit/launchserver/command/service/ClientsCommand.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/command/service/ClientsCommand.java
@@ -8,6 +8,8 @@ import pro.gravit.launchserver.socket.handlers.WebSocketFrameHandler;
 import pro.gravit.utils.helper.IOHelper;
 import pro.gravit.utils.helper.LogHelper;
 
+import java.util.Base64;
+
 public class ClientsCommand extends Command {
     public ClientsCommand(LaunchServer server) {
         super(server);
@@ -34,8 +36,12 @@ public class ClientsCommand extends Command {
                 LogHelper.info("Channel %s | connectUUID %s | checkSign %s", ip, frameHandler.getConnectUUID(), client.checkSign ? "true" : "false");
             else {
                 LogHelper.info("Client name %s | ip %s | connectUUID %s", client.username == null ? "null" : client.username, ip, frameHandler.getConnectUUID());
+                LogHelper.subInfo("userUUID: %s | session %s", client.uuid == null ? "null" : client.uuid.toString(), client.session == null ? "null" : client.session);
                 LogHelper.subInfo("Data: checkSign %s | auth_id %s", client.checkSign ? "true" : "false",
                         client.auth_id);
+                if(client.trustLevel != null) {
+                    LogHelper.subInfo("trustLevel | key %s | pubkey %s", client.trustLevel.keyChecked ? "checked" : "unchecked", client.trustLevel.publicKey == null ? "null" : Base64.getEncoder().encode(client.trustLevel.publicKey));
+                }
                 LogHelper.subInfo("Permissions: %s (permissions %d | flags %d)", client.permissions == null ? "null" : client.permissions.toString(), client.permissions == null ? 0 : client.permissions.permissions, client.permissions == null ? 0 : client.permissions.flags);
             }
         }));
