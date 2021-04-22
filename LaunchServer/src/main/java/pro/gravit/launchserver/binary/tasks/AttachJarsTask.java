2def9c20d9488dcf8e8a6f53302c7c5f36a0bd39
Gravita <gravita@gravit.pro>
2021-04-22 21:03:43 +0700

    [FIX] Support SPI in Launcher

--- LaunchServer/src/main/java/pro/gravit/launchserver/binary/tasks/AttachJarsTask.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/binary/tasks/AttachJarsTask.java
@@ -55,8 +55,13 @@ public class AttachJarsTask implements LauncherBuildTask {
 
     private void attach(ZipOutputStream output, Path inputFile, List<Path> lst) throws IOException {
         for (Path p : lst) {
-            AdditionalFixesApplyTask.apply(inputFile, p, output, srv, (e) -> exclusions.stream().anyMatch(e.getName()::startsWith), false);
+            AdditionalFixesApplyTask.apply(inputFile, p, output, srv, (e) -> filter(e.getName()), false);
         }
+    }
+
+    private boolean filter(String name) {
+        if(name.startsWith("META-INF/services")) return false;
+        return exclusions.stream().anyMatch(name::startsWith);
     }
 
     @Override
