781ab2712777f6d0cfd66d2c362b51a83d1036ca
Tenebrius <magtemtenebrius@gmail.com>
2021-06-14 13:59:53 +0500

    [Fix] Хеширование

--- LaunchServer/src/main/java/pro/gravit/launchserver/auth/password/DigestPasswordVerifier.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/auth/password/DigestPasswordVerifier.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pro.gravit.utils.helper.SecurityHelper;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -17,8 +19,11 @@ public class DigestPasswordVerifier extends PasswordVerifier {
     public boolean check(String encryptedPassword, String password) {
         try {
             MessageDigest digest = MessageDigest.getInstance(algo);
-            byte[] bytes = SecurityHelper.fromHex(encryptedPassword);
-            return Arrays.equals(password.getBytes(StandardCharsets.UTF_8), digest.digest(bytes));
+            digest.update(password.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = digest.digest();
+            String myHash = DatatypeConverter
+                    .printHexBinary(bytes);
+            return myHash.equalsIgnoreCase(encryptedPassword);
         } catch (NoSuchAlgorithmException e) {
             logger.error("Digest algorithm {} not supported", algo);
             return false;
