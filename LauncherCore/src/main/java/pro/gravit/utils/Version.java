86b891e30a7cccea815bf0545591213df335bd16
Gravita <gravita@gravit.pro>
2021-11-18 15:42:38 +0700

    [ANY] 6.0.0 experimental

--- LauncherCore/src/main/java/pro/gravit/utils/Version.java
+++ LauncherCore/src/main/java/pro/gravit/utils/Version.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public final class Version implements Comparable<Version> {
 
-    public static final int MAJOR = 5;
-    public static final int MINOR = 2;
-    public static final int PATCH = 7;
+    public static final int MAJOR = 6;
+    public static final int MINOR = 0;
+    public static final int PATCH = 0;
     public static final int BUILD = 1;
-    public static final Version.Type RELEASE = Type.STABLE;
+    public static final Version.Type RELEASE = Type.EXPERIMENTAL;
     public final int major;
     public final int minor;
     public final int patch;
