b67ae753f0f8d82158a85b7b60d4db30b5bfb9a8
Gravita <gravita@gravit.pro>
2022-03-22 15:37:21 +0700

    [ANY] 5.2.10-dev

--- LauncherCore/src/main/java/pro/gravit/utils/Version.java
+++ LauncherCore/src/main/java/pro/gravit/utils/Version.java
@@ -6,9 +6,9 @@ public final class Version implements Comparable<Version> {
 
     public static final int MAJOR = 5;
     public static final int MINOR = 2;
-    public static final int PATCH = 9;
+    public static final int PATCH = 10;
     public static final int BUILD = 1;
-    public static final Version.Type RELEASE = Type.STABLE;
+    public static final Version.Type RELEASE = Type.DEV;
     public final int major;
     public final int minor;
     public final int patch;
