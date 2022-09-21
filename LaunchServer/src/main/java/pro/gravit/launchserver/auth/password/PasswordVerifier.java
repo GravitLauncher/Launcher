90ee90973e574248a0b910d37ee67d4cb398fee5
Metall <25564543+metallnt@users.noreply.github.com>
2022-09-21 08:40:40 +0500

    Update PasswordVerifier.java
    Добавил:
    1. Способ верификации "django", для осуществления авторизации с помощью PBKDF2 с SHA256.
    2. Исключение на отсутствующий алгоритм(NoSuchAlgorithmException)
    3. Исключение на неверные ключи(InvalidKeySpecException)

--- LaunchServer/src/main/java/pro/gravit/launchserver/auth/password/PasswordVerifier.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/auth/password/PasswordVerifier.java
@@ -2,6 +2,9 @@ package pro.gravit.launchserver.auth.password;
 
 import pro.gravit.utils.ProviderMap;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public abstract class PasswordVerifier {
     public static final ProviderMap<PasswordVerifier> providers = new ProviderMap<>("PasswordVerifier");
     private static boolean registeredProviders = false;
@@ -14,11 +17,12 @@ public abstract class PasswordVerifier {
             providers.register("json", JsonPasswordVerifier.class);
             providers.register("accept", AcceptPasswordVerifier.class);
             providers.register("reject", RejectPasswordVerifier.class);
+            providers.register("django", DjangoPasswordVerifier.class);
             registeredProviders = true;
         }
     }
 
-    public abstract boolean check(String encryptedPassword, String password);
+    public abstract boolean check(String encryptedPassword, String password) throws NoSuchAlgorithmException, InvalidKeySpecException;
 
     public String encrypt(String password) {
         throw new UnsupportedOperationException();
