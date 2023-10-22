b5e10e8f9d6a5a110405537764cfdb970edc2c1f
Gravita <12893402+gravit0@users.noreply.github.com>
2023-10-22 15:27:54 +0700

    [FEATURE] JsonTexture convertMap

--- LaunchServer/src/main/java/pro/gravit/launchserver/auth/texture/JsonTextureProvider.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/auth/texture/JsonTextureProvider.java
@@ -42,24 +42,8 @@ public class JsonTextureProvider extends TextureProvider {
     public Map<String, Texture> getAssets(UUID uuid, String username, String client) {
         try {
             var result = HTTPRequest.jsonRequest(null, "GET", new URL(RequestTextureProvider.getTextureURL(url, uuid, username, client)));
-
             Map<String, JsonTexture> map = Launcher.gsonManager.gson.fromJson(result, MAP_TYPE);
-            if (map == null) {
-                return new HashMap<>();
-            }
-            if (map.get("skin") != null) { // Legacy script
-                map.put("SKIN", map.get("skin"));
-                map.remove("skin");
-            }
-            if (map.get("cloak") != null) {
-                map.put("CAPE", map.get("cloak"));
-                map.remove("cloak");
-            }
-            Map<String, Texture> res = new HashMap<>();
-            for(var e : map.entrySet()) {
-                res.put(e.getKey(), e.getValue().toTexture());
-            }
-            return res;
+            return JsonTexture.convertMap(map);
         } catch (IOException e) {
             logger.error("JsonTextureProvider", e);
             return new HashMap<>();
@@ -69,6 +53,17 @@ public class JsonTextureProvider extends TextureProvider {
     public record JsonTexture(String url, String hash, Map<String, String> metadata) {
         public Texture toTexture() {
             return new Texture(url, hash == null ? null : SecurityHelper.fromHex(hash), metadata);
+        }
+
+        public static Map<String, Texture> convertMap(Map<String, JsonTexture> map) {
+            if (map == null) {
+                return new HashMap<>();
+            }
+            Map<String, Texture> res = new HashMap<>();
+            for(var e : map.entrySet()) {
+                res.put(e.getKey(), e.getValue().toTexture());
+            }
+            return res;
         }
     }
 }
