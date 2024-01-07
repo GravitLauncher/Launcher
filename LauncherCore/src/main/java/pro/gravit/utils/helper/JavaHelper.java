b3eb0ebb9805eb31771600f3c374c2b391bd6cad
Gravita <12893402+gravit0@users.noreply.github.com>
2024-01-07 20:18:15 +0700

    [FIX] Support arch i586 in release file

--- LauncherCore/src/main/java/pro/gravit/utils/helper/JavaHelper.java
+++ LauncherCore/src/main/java/pro/gravit/utils/helper/JavaHelper.java
@@ -257,7 +257,6 @@ public class JavaHelper {
                             arch = JVMHelper.getArch(archProperty);
                         }
                     } catch (Throwable ignored) {
-                        arch = null;
                     }
                     String modulesProperty = getProperty(properties, "MODULES");
                     if(modulesProperty != null) {
