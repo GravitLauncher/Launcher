52c9196dcc9f460dfe6de03bcbedf4403dd56d98
Gravita <12893402+gravit0@users.noreply.github.com>
2024-04-25 03:05:13 +0700

    [FIX] SQLCoreProvider

--- LaunchServer/src/main/java/pro/gravit/launchserver/auth/core/SQLCoreProvider.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/auth/core/SQLCoreProvider.java
@@ -1,10 +1,25 @@
 package pro.gravit.launchserver.auth.core;
 
+import pro.gravit.launchserver.LaunchServer;
+import pro.gravit.launchserver.auth.AuthProviderPair;
 import pro.gravit.launchserver.auth.HikariSQLSourceConfig;
 import pro.gravit.launchserver.auth.SQLSourceConfig;
 
 public class SQLCoreProvider extends AbstractSQLCoreProvider {
     public HikariSQLSourceConfig holder;
+
+    @Override
+    public void close() {
+        super.close();
+        holder.close();
+    }
+
+    @Override
+    public void init(LaunchServer server, AuthProviderPair pair) {
+        holder.init();
+        super.init(server, pair);
+    }
+
     @Override
     public SQLSourceConfig getSQLConfig() {
         return holder;
