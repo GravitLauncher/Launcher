4cdd2039a02d0938c99ff6d30890a44557f26886
Gravita <12893402+gravit0@users.noreply.github.com>
2025-08-28 02:13:27 +0700

    [FIX] Remove debug folder from jar

--- LaunchServer/src/main/java/pro/gravit/launchserver/binary/tasks/MainBuildTask.java
+++ LaunchServer/src/main/java/pro/gravit/launchserver/binary/tasks/MainBuildTask.java
@@ -61,7 +61,7 @@ public class MainBuildTask implements LauncherBuildTask {
             for (Path e : server.launcherBinary.coreLibs) {
                 reader.getCp().add(new JarFile(e.toFile()));
             }
-            context.pushJarFile(inputJar, (e) -> blacklist.contains(e.getName()) || e.getName().startsWith("pro/gravit/launcher/debug/"), (e) -> true);
+            context.pushJarFile(inputJar, (e) -> blacklist.contains(e.getName()) || e.getName().startsWith("pro/gravit/launcher/runtime/debug/"), (e) -> true);
 
             // map for guard
             Map<String, byte[]> runtime = new HashMap<>(256);
