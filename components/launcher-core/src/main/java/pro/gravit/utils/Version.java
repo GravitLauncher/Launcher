24dde95a002da70f4b952d46bc16c75c6d4e52f5
Gravita <12893402+gravit0@users.noreply.github.com>
2025-08-30 11:53:00 +0700

    [ANY] 5.7.1 beta release

--- components/launcher-core/src/main/java/pro/gravit/utils/Version.java
+++ components/launcher-core/src/main/java/pro/gravit/utils/Version.java
@@ -6,7 +6,7 @@ public final class Version implements Comparable<Version> {
 
     public static final int MAJOR = 5;
     public static final int MINOR = 7;
-    public static final int PATCH = 0;
+    public static final int PATCH = 1;
     public static final int BUILD = 1;
     public static final Version.Type RELEASE = Type.BETA;
     public final int major;
