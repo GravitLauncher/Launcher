f07177c3a3ffb52d2b5466348ed3b0253ed87f7d
microwin7 <usa.microwin8@gmail.com>
2026-03-08 00:11:02 +0300

    [FEATURE] Add tab-completion for token server command

--- components/launchserver/src/main/java/pro/gravit/launchserver/command/service/TokenCommand.java
+++ components/launchserver/src/main/java/pro/gravit/launchserver/command/service/TokenCommand.java
@@ -3,11 +3,17 @@ package pro.gravit.launchserver.command.service;
 import io.jsonwebtoken.Jwts;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jline.reader.Candidate;
 import pro.gravit.launchserver.LaunchServer;
 import pro.gravit.launchserver.auth.AuthProviderPair;
 import pro.gravit.launchserver.auth.profiles.ProfilesProvider;
 import pro.gravit.launchserver.command.Command;
 import pro.gravit.utils.command.SubCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TokenCommand extends Command {
     private transient final Logger logger = LogManager.getLogger();
@@ -25,7 +31,8 @@ public class TokenCommand extends Command {
         });
         this.childCommands.put("server", new SubCommand("[profileName] (authId) (public only)", "generate new server token") {
             @Override
-            public void invoke(String... args) {
+            public void invoke(String... args) throws Exception {
+                verifyArgs(args, 1);
                 AuthProviderPair pair = args.length > 1 ? server.config.getAuthProviderPair(args[1]) : server.config.getAuthProviderPair();
                 boolean publicOnly = args.length <= 2 || Boolean.parseBoolean(args[2]);
                 ProfilesProvider.UncompletedProfile profile = null;
@@ -44,6 +51,26 @@ public class TokenCommand extends Command {
                 }
                 String token = server.authManager.newCheckServerToken(profile != null ? profile.getUuid().toString() : args[0], pair.name, publicOnly);
                 logger.info("Server token {} authId {}: {}", args[0], pair.name, token);
+            }
+
+            @Override
+            public List<Candidate> complete(List<String> words, int wordIndex, String word) {
+                return switch (wordIndex) {
+                    case 0 -> server.config.profilesProvider.getProfiles(null).stream()
+                            .map(ProfilesProvider.UncompletedProfile::getName)
+                            .filter(name -> name != null && name.startsWith(word))
+                            .map(Candidate::new)
+                            .collect(Collectors.toList());
+                    case 1 -> server.config.auth.keySet().stream()
+                            .filter(name -> name.startsWith(word))
+                            .map(Candidate::new)
+                            .collect(Collectors.toList());
+                    case 2 -> Stream.of("false", "true")
+                            .filter(s -> s.startsWith(word))
+                            .map(Candidate::new)
+                            .collect(Collectors.toList());
+                    default -> new ArrayList<>();
+                };
             }
         });
     }
