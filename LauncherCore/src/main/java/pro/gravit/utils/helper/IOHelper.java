05d011840931654a5fed8f0a24202a466033cb1c
microwin7 <usa.microwin8@gmail.com>
2026-05-29 21:40:46 +0300

    Revert "[FIX] Fix resolution of files with special characters for embedded file server (#705)"
    This reverts commit b97f1811c080fcfad7dabe19947edbe9ed0273e0.

--- LauncherCore/src/main/java/pro/gravit/utils/helper/IOHelper.java
+++ LauncherCore/src/main/java/pro/gravit/utils/helper/IOHelper.java
@@ -5,15 +5,12 @@ import javax.imageio.ImageReader;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collections;
-import java.util.HexFormat;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -561,34 +558,6 @@ public final class IOHelper {
         } catch (UnsupportedEncodingException e) {
             throw new InternalError(e);
         }
-    }
-
-    public static String urlDecodeStrict(String s) {
-        var builder = new StringBuilder();
-        char[] charArray = s.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
-            char c = charArray[i];
-            if (c != '%') {
-                builder.append(c);
-                continue;
-            }
-
-            if (i + 2 >= charArray.length) {
-                return null;
-            }
-
-            var buffer = UNICODE_CHARSET.decode(ByteBuffer.wrap(HexFormat.of().parseHex(CharBuffer.wrap(charArray, i + 1, 2))));
-
-            builder.append(buffer);
-
-            i += 2;
-        }
-
-        return builder.toString();
-    }
-
-    public static String getPathFromUrlFragment(String urlFragment) {
-        return urlFragment.indexOf('?') < 0 ? urlFragment : urlFragment.substring(0, urlFragment.indexOf('?'));
     }
 
     public static String verifyFileName(String fileName) {
